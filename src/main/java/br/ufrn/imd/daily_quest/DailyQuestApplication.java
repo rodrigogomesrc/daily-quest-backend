0485c191b1722e7d944d5ec27a183c05936c26db
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2024-05-31 14:11:23 -0300

    inicializa projeto

---
+++ src/main/java/br/ufrn/imd/daily_quest/DailyQuestApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.daily_quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufrn/imd/daily_quest/DailyQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.daily_quest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
